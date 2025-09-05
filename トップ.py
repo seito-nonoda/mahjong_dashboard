f4c38b2b75584273d77389aa2963cff22f7041cb
Seito Nonoda <seito.nonoda@gmail.com>
2025-09-05 22:03:29 +0900

    add description to top page

---
+++ トップ.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("麻雀結果集計ツール")
+description = """
+### 麻雀の結果を登録、表示するツールです
+ 機能一覧
+- 📄スコア表
+  - スコア表の形式で結果を見ることができます
+- ✏️スコア入力
+  - スコアを入力することができます
+"""
+st.markdown(description)

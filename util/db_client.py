08184a363485963ba42ea6b6e304e380efb99002
Seito Nonoda <seito.nonoda@gmail.com>
2025-09-06 10:08:13 +0900

    change secret rule

--- util/db_client.py
+++ util/db_client.py
@@ -6,6 +6,7 @@ import streamlit as st
 
 
 SECRET_DIR = "secret"
+SECRET_RULE = "mahjong"
 
 
 def create_db_client():
@@ -16,16 +17,16 @@ def create_db_client():
         if file.is_file():
             secret_file = file.name
 
-    if secret_file is None:
+    if SECRET_RULE in secret_file:
+        # retrieve secret from local file
+        secret_path = secret_path / secret_file
+        cred = credentials.Certificate(str(secret_path))
+
+    else:
         # retrieve secret from streamlit settings
         firebase_creds = st.secrets["firebase_service_account"]
         firebase_creds_dict = dict(firebase_creds)
         cred = credentials.Certificate(firebase_creds_dict)
-
-    else:
-        # retrieve secret from local file
-        secret_path = secret_path / secret_file
-        cred = credentials.Certificate(str(secret_path))
 
     if not firebase_admin._apps:
         firebase_admin.initialize_app(cred)

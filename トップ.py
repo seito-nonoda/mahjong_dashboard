8693ec94c605aebfeab50c9092362bb4f51cde74
Makoto Yamada <woocie.rugby@gmail.com>
2025-09-09 05:03:01 +0900

    add authentication flow with login and logout functionality

--- トップ.py
+++ トップ.py
@@ -1,5 +1,25 @@
 import streamlit as st
 
+from util.auth_utils import get_authenticator
+
+# Create authenticator object
+authenticator = get_authenticator()
+
+authentication_status = st.session_state.get("authentication_status")
+
+if not authentication_status:
+    # Render login widget
+    authenticator.login()
+
+    if authentication_status is False:
+        st.error("ユーザー名またはパスワードが間違っています")
+    elif authentication_status is None:
+        st.warning("ユーザー名とパスワードを入力してください")
+    st.stop()
+
+# Show logout button
+authenticator.logout("ログアウト")
+
 st.title("麻雀結果集計ツール")
 description = """
 ### 麻雀の結果を登録、表示するツールです
